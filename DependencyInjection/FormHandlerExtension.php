1979a10ab5bf4079b381292aeb4a04d4fee00bdb
Marcin Bonk <marvcin@gmail.com>
2020-11-30 14:22:38 +0100

    Typo in extension name

--- DependencyInjection/FormHandlerExtension.php
+++ DependencyInjection/FormHandlerExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class ixFormHandlerExtension extends Extension
+final class FormHandlerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
